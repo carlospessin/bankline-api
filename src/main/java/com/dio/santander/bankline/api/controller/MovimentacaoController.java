64fede2baaddf2d6770fa2039b617e3be20ac139
Carlos Pessin <carlospessin@gmail.com>
2022-05-26 16:12:52 -0300

    add filtro por id

--- src/main/java/com/dio/santander/bankline/api/controller/MovimentacaoController.java
+++ src/main/java/com/dio/santander/bankline/api/controller/MovimentacaoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,7 +20,7 @@ import com.dio.santander.bankline.api.service.CorrentistaService;
 import com.dio.santander.bankline.api.service.MovimentacaoService;
 
 @RestController
-@RequestMapping("/movimentacao")
+@RequestMapping("/movimentacoes")
 public class MovimentacaoController {
 	@Autowired
 	private MovimentacaoRepository repository;
@@ -31,7 +32,10 @@ public class MovimentacaoController {
 	public List<Movimentacao> findAll() {
 		return repository.findAll();
 	}
-	
+	@GetMapping("/{idConta}")
+	public List<Movimentacao> findAll(@PathVariable("idConta") Integer idConta){
+		return repository.findByIdConta(idConta);
+	}
 	@PostMapping
 	public void save(@RequestBody NovaMovimentacao movimentacao) {
 		service.save(movimentacao);
